a5bccf8c5b7183d47de4ed041918ffd9f006b42d
HubuSugar <531982033@qq.com>
2021-10-26 23:42:48 +0800

    回文子串

---
+++ leetcodeEveryDay/src/edu/hubu/hubuSugar/readme.md
@@ -0,0 +1,20 @@
+## leetcode swordoffer extends
+
+### extends
+    一个数的平方根
+### leetcode
+#### week01
+    1.两数之和  
+    2.两数相加  
+    3.无重复数字的最长子串
+    4.寻找两个正序数组的中位数
+
+
+
+#### week02
+
+
+
+
+### swordoffer
+    反转链表
--- leetcodeEveryDay/src/edu/hubu/hubuSugar/week01/LongestPalindrome.java
+++ leetcodeEveryDay/src/edu/hubu/hubuSugar/week01/LongestPalindrome.java
@@ -7,12 +7,47 @@ package edu.hubu.hubuSugar.week01;
  **/
 public class LongestPalindrome {
 
+    /**
+     * 中心扩散
+     * @param s
+     * @return
+     */
     public String longestPalindrome(String s) {
+        if(s == null || s.length() < 2) return s;
+        String result = "";
+        int start = 0;
+        int end = 0;
+        for(int i = 0;i < s.length();i++){
+            int l1 = expandedFromCenter(s ,i,i);
+            int l2 = expandedFromCenter(s,i,i+1);
+            int max = Math.max(l1,l2);
+            if(max > result.length()){
+                // 注意这里起始位置需要将最大长度减一后再取一半
+                start = i - (max - 1) / 2;
+                end = i + max / 2;
+                result = s.substring(start, end + 1);
+            }
+        }
 
-        return "";
+        return result;
+    }
+
+    private int expandedFromCenter(String s,int left,int right){
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+    public boolean isPalindrome(String s){
+
+        return false;
     }
 
     public static void main(String[] args) {
 
+        String abba = new LongestPalindrome().longestPalindrome("abceabbad");
+        System.out.println(abba);
     }
 }
