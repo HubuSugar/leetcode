7cdfb663b63a281970e92164e0df9f9e12292a69
HubuSugar <531982033@qq.com>
2022-03-07 23:45:14 +0800

    最小路径和

---
+++ leetcodeEveryDay/src/edu/hubu/top100/day02/MinPathSum.java
@@ -0,0 +1,41 @@
+package edu.hubu.top100.day02;
+
+
+/**
+ * @Author: huxiaoge
+ * @Date: 2022-03-07
+ * @Description: 最小路径和
+ **/
+public class MinPathSum {
+
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = grid[0][i] + dp[0][i - 1];
+        }
+        for (int j = 1; j < m; j++) {
+            dp[j][0] = grid[j][0] + dp[j - 1][0];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+
+    public static void main(String[] args) {
+        int[][] grid = new int[][]{
+                {1, 3, 1},
+                {1, 5, 1},
+                {4, 2, 1}
+        };
+        System.out.println(new MinPathSum().minPathSum(grid));
+    }
+
+}
