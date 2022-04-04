e98a0f8810e49fb3ef3052a4b0b85c3a1dad9105
HubuSugar <531982033@qq.com>
2022-04-04 10:25:09 +0800

    第k大的元素

---
+++ leetcodeEveryDay/src/edu/hubu/top100/day04/FindKthLargest.java
@@ -0,0 +1,35 @@
+package edu.hubu.top100.day04;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @Author: huxiaoge
+ * @Date: 2022-04-04
+ * @Description:
+ **/
+public class FindKthLargest {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(k, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+        });
+
+        for(int num : nums){
+            //先加入再判断
+            queue.add(num);
+            if(queue.size() > k){
+                queue.poll();
+            }
+        }
+
+        return queue.poll();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{3,2,1,5,6,4};
+        System.out.println(new FindKthLargest().findKthLargest(nums, 2));
+    }
+}
